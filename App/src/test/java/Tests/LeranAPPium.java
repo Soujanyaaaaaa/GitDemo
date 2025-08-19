b0eb2320f9342d62b2e4f2ce3e648784cebbb5a1
Soujanya <soujnadiger99@gmail.com>
2025-08-19 18:04:13 +0530

    push another method

--- App/src/test/java/Tests/LeranAPPium.java
+++ App/src/test/java/Tests/LeranAPPium.java
@@ -12,6 +12,11 @@ System.out.println("I am changing somethings from gitdemo cloned one");
 
 System.out.println("dhskjd f bjsdhuie fd8");
 
-	}
 
+
+	}
+@Test
+public void Test() {
+	System.out.println("I am on my right path");
+}
 }
